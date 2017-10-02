361a7dac34dbb2386bad80e1b124f1c56d216400
User-ПК\User <folomeev-egor@Mail.ru>
2017-10-02 12:13:12 +0300

    изменение прокрутки

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,4 @@
 			body.removeClass("fullscreen-menu");
 			
 		});
-		
-		// Default
-		jQuery.scrollSpeed(100, 800);
-		
-		// Custom Easing
-		jQuery.scrollSpeed(100, 800, 'easeOutCubic');
 })

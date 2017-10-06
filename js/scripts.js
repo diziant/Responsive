cbdde689e615c7fe09732fd894916e3c94340593
User-ПК\User <folomeev-egor@Mail.ru>
2017-10-06 13:16:48 +0300

    изменение скрипта для скрола по якорям

--- js/scripts.js
+++ js/scripts.js
@@ -24,20 +24,20 @@
 	
 	
 	
-		$('.link-scroll:nth-child(1)').click(function(){
-			$('html, body').animate({scrollTop:$('#about').position().top - 80}, 1200);
+		$('.scroll-list-item:nth-child(1)').click(function(){
+			$('html, body').animate({scrollTop:$('#about').position().top - 40}, 1200);
 		});
 		
-		$('.link-scroll:nth-child(2)').click(function(){
-			$('html, body').animate({scrollTop:$('#projects').position().top - 80}, 1200);
+		$('.scroll-list-item:nth-child(2)').click(function(){
+			$('html, body').animate({scrollTop:$('#projects').position().top - 40}, 1200);
 		});
 		
-		$('.link-scroll:nth-child(3)').click(function(){
-			$('html, body').animate({scrollTop:$('#blog').position().top - 80}, 1200);
+		$('.scroll-list-item:nth-child(3)').click(function(){
+			$('html, body').animate({scrollTop:$('#blog').position().top - 40}, 1200);
 		});
 		
-		$('.link-scroll:nth-child(4)').click(function(){
-			$('html, body').animate({scrollTop:$('#contacts').position().top - 80}, 1200);
+		$('.scroll-list-item:nth-child(4)').click(function(){
+			$('html, body').animate({scrollTop:$('#contacts').position().top - 40}, 1200);
 		});
 		
 		/**Переделать с помощью одной функции и вызывом по onclick в html**/
